2786fe391e7a8990577875f21ef4262dbd136129
Kartike Chopra <62751693+Scythektk@users.noreply.github.com>
2023-07-16 23:05:08 +0530

    successful api call

--- superset-frontend/src/JobHome/jobHome.jsx
+++ superset-frontend/src/JobHome/jobHome.jsx
@@ -3,11 +3,27 @@ import MyNavbar from "../Reuse/Navbar.jsx";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import axios from 'axios';
+
 const JobHome = () => {
     const navigate = useNavigate();
+
+    const [allJobs, setAllJobs] = useState(null);
+
+    useEffect(()=>{
+        axios.get('https://better-naukri-com.onrender.com/jobs',{"Content-Type": "application/json"})
+        .then(function (response) {
+            setAllJobs(response.data.jobs);
+            console.log(response.data.jobs)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });    
+    }, [])
+    
     useEffect(() => {
         const loggedInUser = sessionStorage.getItem("UID");
-        if (loggedInUser == null) {
+        if (loggedInUser == null){
             navigate('/')
         }
     }, []);
@@ -15,6 +31,17 @@ const JobHome = () => {
     return ( 
         <div>
             <MyNavbar></MyNavbar>
+            {allJobs &&
+                allJobs.map(job => {
+                    return (
+                        <div key={job.companyId}>
+                            Role: {job.role}
+                            Compensation: {job.compensation}
+                            Location: {job.location}
+                        </div>
+                    )
+                })
+            }
         </div>
     );
     
