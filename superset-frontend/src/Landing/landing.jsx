59c583aa329a69d0d2cfe237d9a4fc5a07c77e2d
Kartike Chopra <62751693+Scythektk@users.noreply.github.com>
2023-07-16 10:35:07 +0530

    landing page final

--- superset-frontend/src/Landing/landing.jsx
+++ superset-frontend/src/Landing/landing.jsx
@@ -1,16 +1,30 @@
 import styles from './landing.module.css';
+import bigRocket from '../assets/bigRocket.png';
+import hire from '../assets/hire.png';
+import job from '../assets/job.png';
 
 const Landing = () => {
     return ( 
         <div className={styles.landingContainer}>
             <div className={styles.headingContainer}>
                 <div className={styles.heading}>
+                    <img src={bigRocket} alt="" />
                     Better Superset
                 </div>
             </div>
             <div className={styles.decisionWrapper}>
-                <div className={styles.hireBtn}><p>I want to hire!</p></div>
-                <div className={styles.jobBtn}><p>I want a job!</p></div>
+                <div className={styles.hireBtn}>
+                    <img src={hire} alt="" />
+                    <p>
+                        I want to hire!
+                    </p>
+                </div>
+                <div className={styles.jobBtn}>
+                    <img src={job} alt="" />
+                    <p>
+                        I want a job!
+                    </p>
+                </div>
             </div>
         </div>
     );
